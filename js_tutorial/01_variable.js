b04334f49cc9ee80a177d239703f6d4a087a54be
Satyam-Chetkar <130957636+Satyam-Chetkar@users.noreply.github.com>
2024-09-15 04:18:07 +0000

    variable few concepts

---
+++ js_tutorial/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 09;
+let emailID = "SATYAM@GMAIL.COM";
+let passWord = "123455";
+let  cityName = "Patna";
+let favouriteFood =
+
+//accountId = 06 not allowed
+/*
+preferred not to use var due to functional scope
+
+we can declare variable with var or without var or let but preferable way is to use let
+and if we dont declare it then its termed as undefined
+*/
+console.log(accountId);
+console.table([accountId,emailID,passWord,cityName,favouriteFood]);
