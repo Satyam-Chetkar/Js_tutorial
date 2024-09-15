6a14d1c112e2b8d28da75bc86dc94253bb359aa7
Satyam Chetkar <root@192.168.1.3>
2024-09-15 13:08:35 +0530

    Done with data types

---
+++ js_tutorial/dataType.js
@@ -0,0 +1,28 @@
+/*"use strict " ;//treat all Js code as newer version
+
+nwe are using node js not browser
+alert("2+3");
+*/
+
+// null -standalone value
+//undefined value
+
+// null is object while undefined data type
+
+//number big init -2 pow 53
+//big init
+//string -""
+//boolean - true or false
+//null -> standlone
+//undefined - >undefined data type
+
+
+
+let name = "Satyam";
+let age = 27;
+let isLoggedIn = true;
+console.log(typeof(age));
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+//https://github.com/Satyam-Chetkar/Js_tutorial.git
