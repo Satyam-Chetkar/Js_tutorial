577c9ef5242ff3738fcd8dd9fd8efe3cd96762d0
Satyam Chetkar <satyam@192.168.1.8>
2024-09-16 12:39:13 +0530

    This is second tutorial on data type

---
+++ js_tutorial/dataType2.js
@@ -0,0 +1,14 @@
+let str1 = "3";
+let str2 = "2";
+console.log(str1 + str2 + 2 + 3);
+console.log(2 + 33 + str1 + str2 ); //predence occur from left to right and +/- has same priority
+
+
+//pre fix and post fix
+
+let number = 23;
+let preNumber = +number;
+
+
+console.log(preNumber);
+
