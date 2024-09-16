cb8edc62c2a55d5dd4c0d25e62e31f701ad34ac8
Satyam Chetkar <satyam@192.168.1.8>
2024-09-16 12:10:03 +0530

    This is conversion

---
+++ js_tutorial/conversion.js
@@ -0,0 +1,23 @@
+/*let score = "33";
+console.log(typeof(score));
+//console.log(number(score));
+let valueInInteger = Number(score);
+console.log(typeof(valueInInteger));
+console.log(valueInInteger);
+
+*/
+
+
+//"33"  -->33
+//"33abc" -->NaN
+// true -->1 false -->0
+
+/*let isLoggedIn = 1;
+let booleanIsLogedIn = Boolean(isLoggedIn);
+console.log(booleanIsLogedIn);*/
+
+// 1 --> true , 0 -->false
+ let score = 23;
+ let scoreInString = String(score);
+ console.log(scoreInString);
+ console.log(typeof(scoreInString));
